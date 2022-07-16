c31193d7efa47763230282bad5be1f8d5c3a8e4c
Benjamin <benjamin.zhao@uwaterloo.ca>
2022-07-16 10:32:10 -0400

    added firebase hosting

--- healthy/src/App.js
+++ healthy/src/App.js
@@ -4,21 +4,21 @@ import { initializeApp } from "firebase/app";
 
 function App() {
 
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: "wec-programming",
+    storageBucket: "wec-programming.appspot.com",
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
