fffa606ded28a22d1b63c4781adc2afcb000adb3
Christopher Leung <minecraft200times@gmail.com>
2022-07-16 11:38:51 -0400

    Created heatmap function
    Started to build heatmap function where it highlights the injuries on the patient's bodies

---
+++ healthy/src/heatmap.js
@@ -0,0 +1,10 @@
+function heatmap(patient, medical){
+    let pSymp = patient.symptoms;
+    for(let i = 0; i < pSymp.length; i++){
+        for(let j = 0; j < medical.length; j++){
+            
+        }
+    }
+}
+
+heatmap(patient,medical);
